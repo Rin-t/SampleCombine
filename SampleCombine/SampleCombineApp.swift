55b06fb95998bf62a640711f69c438df767abebb
Rin-t <rin.t.4921@gmail.com>
2023-05-18 21:57:35 +0900

    Initial Commit

---
+++ SampleCombine/SampleCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCombineApp.swift
+//  SampleCombine
+//
+//  Created by Rin on 2023/05/18.
+//
+
+import SwiftUI
+
+@main
+struct SampleCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
